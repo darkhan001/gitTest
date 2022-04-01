2d6595f6d227ad96d3dc4ac6a814c579ccfd5cf8
Darkhan Kudysbek <darhan.kudysbek01@gmail.com>
2022-04-01 17:40:19 +0600

    m

---
+++ calc.py
@@ -0,0 +1,20 @@
+
+
+def plus(a,b):
+    retrun a+b;
+
+
+def minus(a,b):
+    retrun a-b;
+
+
+def multiply(a,b):
+    retrun a*b;
+
+
+def divade(a,b):
+    retrun a+b;
+
+
+def root(a,b):
+    retrun (a*b)**1/2;
